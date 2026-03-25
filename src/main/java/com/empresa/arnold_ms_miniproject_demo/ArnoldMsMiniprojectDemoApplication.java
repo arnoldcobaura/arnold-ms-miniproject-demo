d5267a08ca6d66eae9513fe4d3cc68ab815a1c9a
Arnold Coba <arnold.coba@transer.com.co>
2026-03-25 18:17:42 -0500

    primer commit proyecto generado desde start.io

---
+++ src/main/java/com/empresa/arnold_ms_miniproject_demo/ArnoldMsMiniprojectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.arnold_ms_miniproject_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArnoldMsMiniprojectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArnoldMsMiniprojectDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/arnold_ms_miniproject_demo/ArnoldMsMiniprojectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.arnold_ms_miniproject_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArnoldMsMiniprojectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
